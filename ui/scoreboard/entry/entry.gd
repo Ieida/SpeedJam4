532e1c56e610a360e13aa2309aebef892d225960
David <37672966+Ieida@users.noreply.github.com>
2024-02-26 20:56:38 +0100

    scoreboard

---
+++ ui/scoreboard/entry/entry.gd
@@ -0,0 +1,9 @@
+extends SplitContainer
+class_name Entry
+
+var username: String
+var score: String
+
+func _ready():
+	$Username.text = username
+	$Score.text = score
---
+++ ui/scoreboard/scoreboard.gd
@@ -0,0 +1,30 @@
+extends Node
+
+var scores: Dictionary
+
+func _enter_tree():
+	load_from_disk()
+
+func _exit_tree():
+	save_to_disk()
+
+func load_from_disk():
+	if not FileAccess.file_exists("user://scores.sb"):
+		return
+	
+	var file = FileAccess.open("user://scores.sb", FileAccess.READ)
+	scores = file.get_var()
+	file.close()
+
+func save_to_disk():
+	if FileAccess.file_exists("user://scores.sb"):
+		if FileAccess.file_exists("user://scores_backup.sb"):
+			DirAccess.remove_absolute("user://scores_backup.sb")
+		DirAccess.rename_absolute("user://scores.sb", "user://scores_backup.sb")
+	
+	var file = FileAccess.open("user://scores.sb", FileAccess.WRITE)
+	file.store_var(scores)
+	file.close()
+
+func add_score(username: String, score: String):
+	scores[username] = score
